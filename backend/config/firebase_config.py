eaa6c583309588a909150e11a7d5c5c82061d758
AdamGrennan <adamgr5000@gmail.com>
2025-05-01 22:48:10 +0100

    Created firebase env key

--- backend/config/firebase_config.py
+++ backend/config/firebase_config.py
@@ -1,11 +1,13 @@
-import os
+import os, json
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+load_dotenv()
+
 def initialize_firebase():
     if not firebase_admin._apps:
-        current_dir = os.path.dirname(__file__)
-        json_path = os.path.join(current_dir, "finalyearproject-35ec5-firebase-adminsdk-fbsvc-933c8356b6.json")
-        cred = credentials.Certificate(json_path)
+        key_data = os.getenv("FIREBASE_KEY")
+        cred = credentials.Certificate(json.loads(key_data))
         firebase_admin.initialize_app(cred)
     return firestore.client()
